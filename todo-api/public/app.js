d1a70a523b0187676f1f88a904c1d449d7f59d4e
nickmly <nickmly@gmail.com>
2018-01-19 09:57:36 -0500

    Create todos with POST request

--- todo-api/public/app.js
+++ todo-api/public/app.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
     $.getJSON("/api/todos").then(function(data){
         addTodos(data);
-    }).catch(function(err){        
-        var errorDiv = $("<div class='error'>" + err + "</div>");
-        $("header").append(errorDiv);
+    }).catch(handleError);
+
+    $("#todoInput").keypress(function(event) {
+        if(event.which == 13) { // if enter pressed
+            createTodo();
+        }
     });
 });
+
+function createTodo(){
+    var todoName = $("#todoInput").val();
+    $.post("/api/todos", {name: todoName}).then(function(data){
+        console.log(data);
+    }).catch(handleError);
+}
+
 
 function addTodos(todos){
     todos.forEach(function(element) {
         var newTodo = $("<li>" + element.name + "</li>");
+        if(element.completed){
+            newTodo.addClass("done");
+        }
         $("ul").append(newTodo);
     });
     
+}
+
+function handleError(err){
+    var errorDiv = $("<div class='error'>" + err + "</div>");
+    $("header").append(errorDiv);
 }
